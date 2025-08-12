aafe1b642b0596fba8846a001e05a4afc9cdf937
sivagopi20025 <93321550+sivagopi20025@users.noreply.github.com>
2025-08-12 15:22:48 -0400

    Update app.py

--- ANN-Classification-Churn/app.py
+++ ANN-Classification-Churn/app.py
@@ -10,13 +10,13 @@ import pickle
 model = tf.keras.models.load_model('model.h5')
 
 # load encoders and scaler 
-with open("onehot_encoder_geo.pkl",'rb') as file:
+with open("ANN-Classification-Churn/onehot_encoder_geo.pkl",'rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
-with open("label_encoder_gender.pkl",'rb') as file:
+with open("ANN-Classification-Churn/label_encoder_gender.pkl",'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open("scaler.pkl",'rb') as file:
+with open("ANN-Classification-Churn/scaler.pkl",'rb') as file:
     scaler = pickle.load(file)
 
 
